b2c86e53dd1cf1d65e6e4120df258c634d162196
Tony <t4tonykuriakose@gmail.com>
2024-12-19 16:46:32 +0530

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      <RowPost title = 'Netflix Originals' linkRef = {NETFLIX_ORIG}/>
       <RowPost title = 'Trending' isSmall linkRef = {TRENDING_URL}/>
       <RowPost title = 'Action' isSmall linkRef = {ACTION_URL}/>
       <RowPost title = 'Comedy' isSmall linkRef = {COMEDY_URL}/>
       <RowPost title = 'Horror' isSmall linkRef = {HORROR_URL}/>
+      <RowPost title = 'Netflix Originals' linkRef = {NETFLIX_ORIG}/>
     </div>
   );
 }
